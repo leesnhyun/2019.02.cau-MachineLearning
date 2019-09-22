b504f1d7cd9258ad1501403d683e5f72ec70f8ca
SengHyun Lee <leesnhyun@gmail.com>
2019-09-22 21:42:07 +0900

    [Assignment01] Concatenate each np-array to use easily

--- pycharm-assignment01/assignment01.py
+++ pycharm-assignment01/assignment01.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import random
+
 
 c1 = np.array([[10, 10]])
 c2 = np.array([[20, 20]])
@@ -9,6 +9,10 @@ N = 500
 u1 = 3.0 * np.random.randn(N, 2)
 u2 = 3.0 * np.random.randn(N, 2)
 
+train_data = np.concatenate((c1+u1, c2+u1), axis=0)
+test_data = np.concatenate((c1+u2, c2+u2), axis=0)
+
+
 def input_plot(g1, g2, title, color):
     plt.title(title)
     plt.scatter(g1[:, 0], g1[:, 1], s=10, color=color[0], alpha=0.5)
@@ -16,8 +20,11 @@ def input_plot(g1, g2, title, color):
     plt.show()
 
 
-input_plot(c1+u1, c2+u1, title='training dataset', color=('blue', 'blue'))
-input_plot(c1+u2, c2+u2, title='testing dataset', color=('red', 'red'))
-input_plot(np.concatenate((c1+u1, c2+u1), axis=0), np.concatenate((c1+u2, c2+u2), axis=0),
-           title="all dataset (overlapped)", color=('blue', 'red'))
+input_plot(train_data[:N, :], train_data[N:, :], title='training dataset', color=('blue', 'blue'))
+input_plot(test_data[:N, :], test_data[N:, :], title='testing dataset', color=('red', 'red'))
+input_plot(train_data, test_data, title="all dataset (overlapped)", color=('blue', 'red'))
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
 
