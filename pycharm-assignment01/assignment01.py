93a47b36ea9f89883580a26efc636398cf968a3e
SengHyun Lee <leesnhyun@gmail.com>
2019-09-23 02:20:29 +0900

    [Assignment01] Apply Grident Descent Algorithm (Partial Derivation)

--- pycharm-assignment01/assignment01.py
+++ pycharm-assignment01/assignment01.py
@@ -4,32 +4,42 @@ import numpy as np
 c1 = np.array([[10, 10, 0]])
 c2 = np.array([[20, 20, 1]])
 
-DIM = 3
 N = 500
+TOTAL = N * 2
 
 u1 = np.concatenate((3.0 * np.random.randn(N, 2), np.zeros((N, 1))), axis=1)
 u2 = np.concatenate((3.0 * np.random.randn(N, 2), np.zeros((N, 1))), axis=1)
 
-train_data = np.concatenate((c1 + u1, c2 + u1), axis=0)
-test_data = np.concatenate((c1 + u2, c2 + u2), axis=0)
+train_data = np.concatenate((c1 + u1, c2 + u1), axis=0).T
+test_data = np.concatenate((c1 + u2, c2 + u2), axis=0).T
 
 
 def input_plot(g1, g2, title, color):
     plt.title(title)
-    plt.scatter(g1[:, 0], g1[:, 1], s=10, color=color[0], alpha=0.5)
-    plt.scatter(g2[:, 0], g2[:, 1], s=10, color=color[1], alpha=0.5)
+    plt.scatter(g1[0, :], g1[1, :], s=10, color=color[0], alpha=0.5)
+    plt.scatter(g2[0, :], g2[1, :], s=10, color=color[1], alpha=0.5)
     plt.show()
 
 
-input_plot(train_data[:N, :], train_data[N:, :], title='training dataset', color=('blue', 'blue'))
-input_plot(test_data[:N, :], test_data[N:, :], title='testing dataset', color=('red', 'red'))
-input_plot(train_data, test_data, title="all dataset (overlapped)", color=('blue', 'red'))
+def output_plot(g1, g2, title, color):
+    plt.title(title)
+    plt.plot(np.arange(1, len(g1)), g1, color=color[0])
+    plt.plot(np.arange(1, len(g2)), g2, color=color[1])
+    plt.show()
+
+
+input_plot(train_data[:2, :N], train_data[:2, N:], title='training dataset', color=('blue', 'blue'))
+input_plot(test_data[:2, :N], test_data[:2, N:], title='testing dataset', color=('red', 'red'))
+input_plot(train_data[:2, :], test_data[:2, :], title="all dataset (overlapped)", color=('blue', 'red'))
 
 
 def binary_classify(data):
-    learning_rate = 0.5
+    learning_rate = 0.015
     w = np.array([0, 0])  # u, v
     b = 0
+
+    losses = []
+    accuracies = []
 
     def init():
         return
@@ -38,27 +48,46 @@ def binary_classify(data):
         return 1 / (1 + np.exp(-z))
 
     def distance(prob, ans):
-        return -(ans * np.log(prob) + (1 - ans) * np.log(1 - prob))
+        return -(np.nan_to_num(ans * np.log(prob)) + np.nan_to_num((1 - ans) * np.log(1 - prob)))
 
-    def loss():
-        return 0
+    def loss(prob, ans):
+        return (1 / TOTAL) * np.nan_to_num(np.sum(distance(prob, ans)))
 
     def accuracy():
         return 0
 
     def dw(z):
-        return (1 / N) * np.sum(data[:, 0:1] * (sigmoid(z) - data[:, 2]))
+        return (1 / TOTAL) * np.sum(data[:2, :] * (sigmoid(z) - data[2, :]), axis=1)
+
+    def db(z):
+        return (1 / TOTAL) * np.sum(sigmoid(z) - data[2, :])
 
     def iterate():
-        nonlocal w, b
+        p_loss = 0
+        nonlocal w, b, losses, accuracies
 
         while True:
-            w = w - learning_rate * (dw(np.dot(w.T, data) + b) - data[:, 2])
+            z = np.dot(w.T, data[:2, :]) + b
+            w = w - (learning_rate * dw(z))
+            b = b - (learning_rate * db(z))
 
-            break
+            n_loss = loss(sigmoid(z), data[2, :])
+            losses.append(n_loss)
+
+            if n_loss == p_loss:
+                break
+            else:
+                print(n_loss)
+                p_loss = n_loss
+                continue
 
     init()
     iterate()
 
+    return losses
 
-binary_classify()
+
+train_loss = binary_classify(train_data)
+test_loss = binary_classify(test_data)
+
+output_plot(train_loss, test_loss, "loss", ('blue', 'red'))
