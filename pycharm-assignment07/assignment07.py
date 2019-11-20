31909153b644b7263957039d5c9eb3f2f584b397
SengHyun Lee <leesnhyun@gmail.com>
2019-11-21 02:45:13 +0900

    [assignment07] write pytorch code

--- pycharm-assignment07/assignment07.py
+++ pycharm-assignment07/assignment07.py
@@ -51,7 +51,7 @@ print("device count : %s" % (torch.cuda.device_count()))
 print("device name : %s" % (torch.cuda.get_device_name(0)))
 print("CUDA available? : %s" % (torch.cuda.is_available()))
 
-batch_size = 300
+batch_size = 30
 
 class Linear(nn.Module):
 
@@ -62,15 +62,15 @@ class Linear(nn.Module):
         self.number_class = num_classes
 
         _size_image = 100 * 100
-        _num1 = 100
-        _num2 = 150
+        _num1 = 150
+        _num2 = 50
 
         self.fc1 = nn.Linear(_size_image, _num1, bias=True)
         self.fc2 = nn.Linear(_num1, _num2, bias=True)
         self.fc3 = nn.Linear(_num2, num_classes, bias=True)
 
-        self.fc_layer1 = nn.Sequential(self.fc1, nn.ReLU(True))
-        self.fc_layer2 = nn.Sequential(self.fc2, nn.ReLU(True))
+        self.fc_layer1 = nn.Sequential(self.fc1, nn.LeakyReLU(True))
+        self.fc_layer2 = nn.Sequential(self.fc2, nn.LeakyReLU(True))
         self.fc_layer3 = nn.Sequential(self.fc3, nn.Sigmoid())
 
         self.classifier = nn.Sequential(self.fc_layer1, self.fc_layer2, self.fc_layer3)
@@ -144,10 +144,24 @@ model.cuda()
 # -----------------------------------------------------------------------------
 # optimization algorithm
 # -----------------------------------------------------------------------------
-learning_rate = 0.05
-optimizer = optim.SGD(model.parameters(), lr=learning_rate, weight_decay=10e-5)
+learning_rate = 0.5
+optimizer = optim.SGD(model.parameters(), lr=learning_rate, weight_decay=0.09)
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=10, min_lr=0.001, verbose=True)
 objective = nn.CrossEntropyLoss()
+
+
+# -----------------------------------------------------------------------------
+# function for draw table
+# -----------------------------------------------------------------------------
+def output_frame_plot(tloss, vloss, tacc, vacc, title):
+    print(" << %s >>" % title)
+    print("------------------------------------------------------")
+    print("                |   %10s    |    %10s     |" % ('loss', 'accuracy'))
+    print("------------------------------------------------------")
+    print("training        |   %.10f   |    %.10f    |" % (tloss, tacc))
+    print("------------------------------------------------------")
+    print("validation      |   %.10f   |    %.10f    |" % (vloss, vacc))
+    print("------------------------------------------------------")
 
 
 # -----------------------------------------------------------------------------
@@ -158,10 +172,13 @@ def train():
     # print('train the model at given epoch')
 
     loss_train = []
+    acc_train = []
 
     model.train()
 
     for idx_batch, (data, target) in enumerate(loader_train):
+
+        correct = 0
 
         data, target = data.cuda(), target.cuda()
         data, target = Variable(data).cuda(), Variable(target).cuda()
@@ -177,10 +194,15 @@ def train():
         loss_train_batch = loss.item() / len(data)
         loss_train.append(loss_train_batch)
 
+        pred = output.data.max(1)[1]
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+        acc_train.append(100. * float(correct) / len(data))
+
     loss_train_mean = np.mean(loss_train)
     loss_train_std = np.std(loss_train)
+    acc_train_mean = np.mean(acc_train)
 
-    return {'loss_train_mean': loss_train_mean, 'loss_train_std': loss_train_std}
+    return {'loss_train_mean': loss_train_mean, 'loss_train_std': loss_train_std, 'accuracy_train_mean':acc_train_mean}
 
 
 # -----------------------------------------------------------------------------
@@ -216,18 +238,28 @@ def test():
 # iteration for the epoch
 # -----------------------------------------------------------------------------
 
-loss_train_mean, loss_train_std, loss_test, accuracy_test = [], [], [], []
+loss_train_mean, loss_train_std, loss_test, accuracy_test, accuracy_train = [], [], [], [], []
+prev_train_loss, next_train_loss = 0, 0
 
 for e in range(1000):
     result_train = train()
     result_test = test()
 
+    next_train_loss = result_train['loss_train_mean']
+
     loss_train_mean.append(result_train['loss_train_mean'])
     loss_train_std.append(result_train['loss_train_std'])
     loss_test.append(result_test['loss_test'])
+
+    accuracy_train.append(result_train['accuracy_train_mean'])
     accuracy_test.append(result_test['accuracy_test'])
 
-    scheduler.step(result_test['accuracy_test'], e)
+    print("[epoch %s] loss: (training) %s, (testing) %s, accuracy: (training) %s, (testing) %s" %
+          (e, loss_train_mean[-1], loss_test[-1], accuracy_train[-1], accuracy_test[-1]))
 
-    print(result_train)
-    print(result_test)
+    if abs(next_train_loss - prev_train_loss) < 10e-7:
+        output_frame_plot(loss_train_mean[-1], loss_test[-1], accuracy_train[-1], accuracy_test[-1], "Result")
+        break
+    else:
+        prev_train_loss = next_train_loss
+        scheduler.step(result_test['accuracy_test'], e)
